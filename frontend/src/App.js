8360120e3a016c407f75570caa273c0152058e79
Prathit <prathit.shivade@gmail.com>
2023-12-31 09:22:11 +0000

    image_gallery:0.0.6     - First API call to Unsplash: Get the image based on search text.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+// UNSPLASH KEY TO MAKE REQUESTS
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
+
 const App = () => {
   const [word, setWord] = useState("");
-  
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setWord("");
   };
-  
+
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search word={ word } setWord={ setWord } handleSubmit={ handleSearchSubmit }/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 };
