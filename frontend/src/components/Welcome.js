d1e9dbd1a47b4164503bf4539eb163fb29cbe096
Prathit <prathit.shivade@gmail.com>
2024-02-11 12:27:02 +0000

    image_gallery:0.3.7     - Update style for Welcome component.

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -3,20 +3,60 @@ import { Container, Button } from "react-bootstrap";
 
 const Welcome = () => {
   const welcomeStyle = {
-    marginTop: "40px",
+    marginTop: "70px",
+    paddingTop: "30px",
+    paddingLeft: "30px",
+    paddingRight: "30px",
     backgroundColor: "#eeeeee",
+  };
+
+  const buttonStyle = {
+    position: "relative",
+    marginLeft: "90%",
+    bottom: "40px",
   };
   return (
     <Container style={welcomeStyle}>
-      <h1>Welcome Images Gallery</h1>
-      <p>
-        This is a simple application that retrieves photos using Unsplash API.
-      </p>
-      <p>
-        In order to start, please enter any search term in the input field.
+      <h1
+        style={{
+          textAlign: "center",
+          paddingTop: "20px",
+          paddingBottom: "20px",
+        }}
+      >
+        Welcome Images Gallery
+      </h1>
+      <ul>
+        <li
+          style={{
+            paddingBottom: "10px",
+          }}
+        >
+          This is a simple application that retrieves photos using Unsplash API.
+          In order to start, please enter any search term in the input field.
+        </li>
+        <li
+          style={{
+            paddingBottom: "10px",
+          }}
+        >
+          When you like an image, you can save the image by clicking on the Save
+          button.
+        </li>
+        <li
+          style={{
+            paddingBottom: "10px",
+          }}
+        >
+          When you would like to remove a saved photo, simply click the Delete
+          button.
+        </li>
+      </ul>
+      <h3 style={{ textAlign: "center", paddingTop: "20px" }}>
         Happy searching...!
-      </p>
-      <Button variant="primary" href="https://unsplash.com" target="_blank">
+      </h3>
+      <br></br>
+      <Button style={buttonStyle} href="https://unsplash.com" target="_blank">
         Learn More
       </Button>
     </Container>
