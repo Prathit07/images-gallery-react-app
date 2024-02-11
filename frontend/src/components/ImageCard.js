bef5b1777448f42227a5a382aea58d68da85ab55
Prathit <prathit.shivade@gmail.com>
2024-02-11 12:28:02 +0000

    image_gallery:0.3.8     - Introduce footer showin artist name and portfolio website link.

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Nav } from "react-bootstrap";
 
 const capitaliseFirstLetter = (text) => {
   if (!text) return ""; // Handle empty or undefined input
@@ -11,6 +11,10 @@ const ImageCard = (props) => {
   const title = props.image.title;
   const description = capitaliseFirstLetter(props.image.description);
   const alt_description = capitaliseFirstLetter(props.image.alt_description);
+  const image_created_by = props.image.user.name
+    ? props.image.user.name
+    : "No Author Name";
+  const portfolio_url = props.image.user.portfolio_url;
   const url = props.image.urls["small"];
   return (
     <Card style={{ width: "18rem", marginTop: "20px" }}>
@@ -26,6 +30,21 @@ const ImageCard = (props) => {
         <Button variant="primary" onClick={() => props.deleteImage(id)}>
           Delete
         </Button>
+        <br></br>
+        <br></br>
+        <Card.Footer className="text-center text-muted">
+          {portfolio_url ? (
+            <Nav className="justify-content-center">
+              <Nav.Item>
+                <Nav.Link href={portfolio_url} target="_blank">
+                  {image_created_by}
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          ) : (
+            <p>{image_created_by}</p>
+          )}
+        </Card.Footer>
       </Card.Body>
     </Card>
   );
